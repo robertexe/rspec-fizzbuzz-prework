68b3f522245c0f95ca07f7b8cdf92fa50f397e83
Robert Tabares <robtabares92@gmail.com>
2018-04-24 17:39:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int%3 && int%5
+  if int%3==0 && int%5==0
     return "FizzBuzz"
-  elsif int%3
+  elsif int%3==0
     return "Fizz"
-  elsif int%5
+  elsif int%5==0
     return "Buzz"
   else
     return nil
